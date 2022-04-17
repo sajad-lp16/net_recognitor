11b59a6f71a7158d0bd785f0a1ae26e4dd97966e
Sajad <sajad.tohidi76@gmail.com>
2022-04-17 10:12:35 +0430

    remove ipregistry source and switch to chrome driver

--- ip_manager/crawlers/main_crawlers.py
+++ ip_manager/crawlers/main_crawlers.py
@@ -6,13 +6,20 @@ from django.conf import settings
 
 from celery import shared_task
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 from . import data_managers
+
+
+def get_web_driver():
+    capa = DesiredCapabilities.CHROME
+    capa["pageLoadStrategy"] = "none"
+    return webdriver.Chrome(desired_capabilities=capa)
 
 
 @shared_task
@@ -29,10 +36,8 @@ def crawl_ip_info(ips, many=False):
     json_data_button = (
         "/html/body/div[1]/div/div[2]/div[2]/div/div[1]/div/div[2]/button"
     )
-    options = Options()
-    options.set_preference("dom.webnotifications.enabled", False)
 
-    web = webdriver.Firefox(options=options)
+    web = get_web_driver()
 
     with web as driver:
         wait = WebDriverWait(driver, timeout=15)
@@ -67,10 +72,7 @@ def crawl_ip_data(ips, many=False):
     ip_input_path = '//*[@id="searchIP"]'
     submit_butt_path = '//*[@id="searchIPButton"]'
 
-    options = Options()
-    options.set_preference("dom.webnotifications.enabled", False)
-
-    web = webdriver.Firefox(options=options)
+    web = get_web_driver()
 
     with web as driver:
         wait = WebDriverWait(driver, timeout=10)
@@ -109,39 +111,6 @@ def crawl_ip_data_api(ips, many=False):
 
 
 @shared_task
-def crawl_ip_registry(ips, many=False):
-    url = "https://ipregistry.co/"
-    ip_input_path = '//*[@id="iptocheck"]'
-    submit_butt_path = '//*[@id="ipcheck_submit"]'
-
-    options = Options()
-    options.set_preference("dom.webnotifications.enabled", False)
-
-    web = webdriver.Firefox(options=options)
-
-    with web as driver:
-        wait = WebDriverWait(driver, timeout=10)
-        driver.get(url)
-        wait.until(presence_of_element_located((By.XPATH, ip_input_path)))
-        wait.until(presence_of_element_located((By.XPATH, submit_butt_path)))
-
-        ip_box = driver.find_element(By.XPATH, ip_input_path)
-        submit_button = driver.find_element(By.XPATH, submit_butt_path)
-
-        if many:
-            assert isinstance(ips, list)
-            for ip in ips:
-                ip_box.send_keys(ip)
-                submit_button.click()
-                # call ip_data_store
-                sleep(3)
-        else:
-            ip_box.send_keys(ips)
-            submit_button.click()
-            return  # ip data store
-
-
-@shared_task
 def crawl_myip(ips, many=False):
     url = "https://myip.ms"
     ip_input_path = '//*[@id="global_txt"]'
@@ -152,10 +121,7 @@ def crawl_myip(ips, many=False):
     login_submit_button_class_name = "ui-button-text"
     dialog_id = "uidialog"
 
-    options = Options()
-    options.set_preference("dom.webnotifications.enabled", False)
-
-    web = webdriver.Firefox(options=options)
+    web = get_web_driver()
 
     with web as driver:
         wait = WebDriverWait(driver, timeout=10)
@@ -220,10 +186,7 @@ def crawl_ripe(ips, many=False):
         "/html/body/header/div/div/div[3]/div[1]/div/div[1]/form/div/button"
     )
 
-    options = Options()
-    options.set_preference("dom.webnotifications.enabled", False)
-
-    web = webdriver.Firefox(options=options)
+    web = get_web_driver()
 
     with web as driver:
         wait = WebDriverWait(driver, timeout=10)
--- ip_manager/utils/mapper.py
+++ ip_manager/utils/mapper.py
@@ -4,7 +4,6 @@ from ip_manager.crawlers import main_crawlers
 CRAWLERS_MAPPER = {
     "default": partial(main_crawlers.crawl_ip_info),
     "ipinfo": partial(main_crawlers.crawl_ip_info),
-    "ipregistry": partial(main_crawlers.crawl_ip_registry),
     "ipdata": partial(main_crawlers.crawl_ip_data),
     "ipdata_api": partial(main_crawlers.crawl_ip_data_api),
     "myip": partial(main_crawlers.crawl_myip),
